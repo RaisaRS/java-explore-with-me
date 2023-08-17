de7fc115c0f6a13f17c3df9947ba3f18b6c7d71a
Raisa <raenka07@mail.ru>
2023-08-17 23:03:25 +0300

    test3

---
+++ service-main/src/main/java/ru/practicum/explore/EvmMainApp.java
@@ -0,0 +1,11 @@
+package ru.practicum.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvmMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EvmMainApp.class, args);
+    }
+}
