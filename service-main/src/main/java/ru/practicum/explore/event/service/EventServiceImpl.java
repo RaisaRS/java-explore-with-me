8914732ea52a68614c618d5595fabe6920d025d0
Raisa <raenka07@mail.ru>
2023-08-25 13:27:09 +0300

    diplom 2 test 2

--- service-main/src/main/java/ru/practicum/explore/event/service/EventServiceImpl.java
+++ service-main/src/main/java/ru/practicum/explore/event/service/EventServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.explore.event.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
